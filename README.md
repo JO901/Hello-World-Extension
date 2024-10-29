337c936c7b1676b772f14770a439dfd0724b4713
Rachel Dean <rchldn@gmail.com>
2024-10-29 12:29:14 -0400

    images loop working, some small updates, added input boxes

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ An extension to remind you to stretch!
 
 stretch goals features 
 -rename extension "Stretch Goals"
--images 
+-images, 
 -options for how often? or user inputs how often they want it to be
 -timer for each stretch type 
 -automatically start the app when the browser starts https://developer.chrome.com/docs/extensions/reference/api/runtime#concepts and https://developer.chrome.com/extensions/runtime#event-onStartup (call chrome API to add an event listener for onStart so we can do this)
@@ -24,7 +24,7 @@ MVP
         -Using DOM manipulation to update the counter in the html file 
 
         
-    -Every Hour remind the user to stretch 
+[Complete]    -Every Hour remind the user to stretch 
         Create a global timer: start the app/ start app on browser opening 
 
 [Complete] -have the counter count down instead of up
--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.alarms.create('ALARM_NAME', {
   when: Date.now(), // when we want it to start counting down from
-  periodInMinutes: 0.1, // when the alarm will run
+  periodInMinutes: 1, // when the alarm will run
   // delayInMinutes: //not sure how this one works - “specifies if the onAlarm event should delay a little before firing”
 });
 
@@ -8,7 +8,8 @@ chrome.alarms.onAlarm.addListener((alarm) => {
   console.log('Alarm working?');
   if (alarm.name === 'ALARM_NAME') {
     console.log('The alarm was named correctly');
-    chrome.notifications.create('NOTFICATION_ID', {
+
+    chrome.notifications.create('NOTIFICATION_ID', {
       // name the notification
       type: 'basic', // other options = image, list, progress
       iconUrl: '/assets/wave.png', // path to icon that should be shown in notification/ relative to root of extension
@@ -25,5 +26,10 @@ chrome.alarms.onAlarm.addListener((alarm) => {
       ],
       // silent: false, // that’s the default setting so it should make a sound
     });
+    chrome.notifications
+      .clear('NOTIFICATION_ID')
+      .then(console.log('notification cleared'))
+      .catch(console.log('error'));
   }
+//   console.log(NOTIFICATION_ID.buttons[0].title); // throws error that NOTIFICATION_ID is not defined; we're trying to access that button click so that we can add a listener to say 'go open the extension if they click 'start stretching''
 });
